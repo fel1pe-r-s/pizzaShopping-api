598e993b5fb886e8c3cb3362b9d84323df330847
fel1pe-r-s <1felipersilva1@gmail.com>
2024-04-10 23:23:49 -0300

    Get profile

--- src/http/auth.ts
+++ src/http/auth.ts
@@ -28,5 +28,16 @@ export const auth = new Elysia()
       signOut: () => {
         auth.remove();
       },
+      getCurrentUser: async () => {
+        const payload = await jwt.verify(auth.value);
+        if (!payload) {
+          throw new Error("Unauthorized");
+        }
+
+        return {
+          userId: payload.sub,
+          restaurantId: payload.restaurantId,
+        };
+      },
     };
   });
